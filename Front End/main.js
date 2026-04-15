a3c0537c25131b2c671dcea6ee064349f84dbf6e
Hysterical-Mafia <hekmatullahr.hm@gmail.com>
2026-04-15 12:05:58 -0400

    Finished with the text content
    created the const status to allow the "searching for" text to be around the header rather than in the output field

--- Front End/main.js
+++ Front End/main.js
@@ -2,24 +2,26 @@ const input = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-btn");
 const output = document.getElementById("output-section");
 
+const status = document.getElementById("status");
+
 function updateOutput(text) {
-    output.textContent = text;
+    status.textContent = text;
 }
-searchBtn.addEventListener("click", async function() {
+    searchBtn.addEventListener("click", async function() {
     const keyword = input.value; 
 
     if (keyword.trim() == "") {
         alert("my input has no value");
         return;
     }
-    updateOutput("Searching for: " + keyword)
+    updateOutput("Searching for: " + keyword);
 
     const url = "http://localhost:3000/search?keyword=" + keyword;  
     const response = await fetch(url);
     const data = await response.json();
     
     const posts = data.data.children;
-
+    output.innerHTML = "";
     for (let i = 0; i < posts.length; i++) {
         const post = posts[i];
         const title = post.data.title;
@@ -27,9 +29,9 @@ searchBtn.addEventListener("click", async function() {
         const newDiv = document.createElement("div");
         newDiv.id = "item" + i ;
         newDiv.className = "post";
-
         newDiv.textContent = title;
-        output.appendChild(newDiv)
+
+        output.appendChild(newDiv);
     }
 
 });
